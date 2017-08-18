fe3eb91746117fb20a06b6c95a13ae15083c9040
etbourreau <et.bourreau@laposte.net>
2017-08-18 16:40:06 +0200

    base application

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: "bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Devfest Conferences Management App"
+        })
+    ]
+};
